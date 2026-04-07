28d37a34f99578bfe8f87269017c24e4805c46f7
unknown <rutujaw.2005@gmail.com>
2026-04-07 21:46:58 +0530

    Update project

--- backend/bias/mitigation.py
+++
@@ -1,6 +0,0 @@
-def remove_sensitive_feature(X_train, X_test, sensitive_col):
-    if sensitive_col in X_train.columns:
-        X_train = X_train.drop(columns=[sensitive_col])
-    if sensitive_col in X_test.columns:
-        X_test = X_test.drop(columns=[sensitive_col])
-    return X_train, X_test
